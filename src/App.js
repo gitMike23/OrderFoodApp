df09ffbb7f0a43f08f6ff95302bd8f1e1572c8a3
Mikhail Medovnik <mmedovnik@mow-mbp-21-1451.local>
2022-02-18 21:29:29 +0300

    Cart modal added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [isCartShown, setIsCartShown] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartShown(true)
+  };
+
+  const hideCartHandler = ()=> {
+    setIsCartShown(false)
+  };
+
   return <Fragment>
-    <Header/>
+    {isCartShown && <Cart onClose={hideCartHandler}/>}
+    <Header onShowCart={showCartHandler} onHideCart={hideCartHandler}/>
     <main>
       <Meals />
     </main>
